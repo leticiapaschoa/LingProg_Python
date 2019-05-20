bf0302cff86638a10f2c8b19af0065cac115373c
Leticia Paschoa da Silva <leticiaps@ciandt.com>
2019-05-20 12:00:29 -0300

    API usando Flask

--- Exercicios/Conversoes_Python/Temperatura.py
+++
@@ -1,5 +0,0 @@
-tempGrausCelsius = float(input('Digite a temperatura em Graus Celsius: '))
-
-tempGrausFarenheit = 1.8*tempGrausCelsius+32
-
-print('Temperatura em Farenheit: ' + str(tempGrausFarenheit))
